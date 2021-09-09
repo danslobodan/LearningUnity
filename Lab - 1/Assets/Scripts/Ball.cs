292e61483a1f00e0e5c616432897808e2dc60921
Slobodan Dan <danastroman@gmail.com>
2021-09-09 23:58:43 +0200

    Added Lab 1 project.

---
+++ Lab - 1/Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 destination;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != destination)
+        {
+            Debug.Log(destination);
+            transform.position = new Vector3(destination.x, transform.position.y, destination.z);
+        }
+    }
+
+    public void OnTilePress(Vector3 destination)
+    {
+        this.destination = destination;
+    }
+}
---
+++ Lab - 1/Assets/Scripts/MoveBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Ball ball;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnMouseUp()
+    {
+        ball.OnTilePress(transform.position);
+    }
+}

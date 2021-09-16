47da7e9d2a02a4d2c043b394058c797575a78ce0
Slobodan Dan <danastroman@gmail.com>
2021-09-16 23:06:20 +0200

    Refactored NodeFromWorldPoint.

--- Lab - 1/Assets/Scripts/Grid.cs
+++ Lab - 1/Assets/Scripts/Grid.cs
@@ -128,17 +128,20 @@ namespace Assets.Scripts
 
         public Node NodeFromWorldPoint(Vector3 worldPosition)
         {
-            float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
-            float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
-
-            percentX = Mathf.Clamp01(percentX);
-            percentY = Mathf.Clamp01(percentY);
-
-            int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
-            int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+            int x = GridPositionFromCoordinate(worldPosition.x, gridWorldSize.x, gridSizeX);
+            int y = GridPositionFromCoordinate(worldPosition.z, gridWorldSize.y, gridSizeY);
 
             return grid[x, y];
         }
+
+        private int GridPositionFromCoordinate(float position, float worldSize, int gridSize)
+        {
+            float percent = (position + worldSize / 2) / worldSize;
+            float normalized = Mathf.Clamp01(percent);
+            int gridPosition = Mathf.RoundToInt((gridSize - 1) * normalized);
+            return gridPosition;
+        }
+
 
         private void Update()
         {

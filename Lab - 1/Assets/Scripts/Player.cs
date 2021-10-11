4f9b61fc16580e4486fb7809e4eb3fe599feee73
Slobodan Dan <danastroman@gmail.com>
2021-10-11 16:17:54 +0200

    Fixed player movement.

--- Lab - 1/Assets/Scripts/Player.cs
+++ Lab - 1/Assets/Scripts/Player.cs
@@ -10,54 +10,92 @@ namespace Assets.Scripts
     public class Player : MonoBehaviour
     {
         public float speed = 5f;
-        public float maxDaylight = 100f;
-        public float daylightSpeed = 1f;
+        private float tolerance = 0.1f;
 
         public Scoreboard scoreboard;
 
-        Vector3[] path;
-        int targetIndex;
-
-        void Update()
-        {
-            if (path == null || path.Length == 0 || targetIndex >= path.Length
-                || scoreboard.Daylight <= 0)
-                return;
-
-            Vector3 currentWaypoint = new Vector3(path[targetIndex].x, transform.position.y, path[targetIndex].z); ;
-            if (transform.position != currentWaypoint)
-            {
-                transform.position = Vector3.MoveTowards(
-                    transform.position,
-                    currentWaypoint,
-                    speed * Time.deltaTime);
-            }
-
-            if (transform.position == currentWaypoint && targetIndex < path.Length)
-                targetIndex++;
-
-            scoreboard.SpendDaylight();
-        }
+        private Vector3[] path;
+        private Vector3 currentWaypoint;
+        private int currentWaypointIndex;
 
         public void OnPathFound(Vector3[] path, bool success)
         {
-            if (success)
+            if (success && path.Length > 0)
             {
                 this.path = path;
-                targetIndex = 0;
+                currentWaypointIndex = 0;
+                UpdateCurrentWaypoint();
             }
+        }
+
+        private void Update()
+        {
+            if (!HasPath() || TargetReached() || !HasDaylight())
+                return;
+
+            Move();
+            scoreboard.SpendDaylight();
+
+            if (WaypointReached(currentWaypoint))
+                NextWaypoint();
+        }
+
+        private bool HasPath()
+        {
+            // Debug.Log($"Has path: {path != null && path.Length > 0}");
+            return path != null && path.Length > 0;
+        }
+
+        private bool TargetReached()
+        {
+            // Debug.Log($"Target reached: {currentWaypointIndex >= path.Length}");
+            return currentWaypointIndex >= path.Length;
+        }
+
+        private bool HasDaylight()
+        {
+            // Debug.Log($"Has daylight: {scoreboard.Daylight > 0}");
+            return scoreboard.Daylight > 0;
+        }
+
+        private bool WaypointReached(Vector3 currentWaypoint)
+        {
+            return Math.Abs(transform.position.x - currentWaypoint.x) < tolerance
+                && Math.Abs(transform.position.z - currentWaypoint.z) < tolerance;
+        }
+
+        private void NextWaypoint()
+        {
+            currentWaypointIndex++;
+            if (TargetReached())
+                return;
+
+            UpdateCurrentWaypoint();
+        }
+
+        private void UpdateCurrentWaypoint()
+        {
+            currentWaypoint = new Vector3(path[currentWaypointIndex].x, transform.position.y, path[currentWaypointIndex].z);
+        }
+
+        private void Move()
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                currentWaypoint,
+                speed * Time.deltaTime);
         }
 
         public void OnDrawGizmos()
         {
             if (path != null)
             {
-                for (int i = targetIndex; i < path.Length; i++)
+                for (int i = currentWaypointIndex; i < path.Length; i++)
                 {
                     Gizmos.color = Color.green;
                     Gizmos.DrawCube(path[i], new Vector3(1, 0.2f, 1));
 
-                    if (i == targetIndex)
+                    if (i == currentWaypointIndex)
                     {
                         Gizmos.DrawLine(transform.position, path[i]);
                     }
